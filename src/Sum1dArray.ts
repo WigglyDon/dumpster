94357cff98acfdad3e5c3b28a21d344c04f5f05e
WigglyDon <donfather1000@gmail.com>
2024-10-01 12:56:24 -0600

    solve sumarray

---
+++ src/Sum1dArray.ts
@@ -0,0 +1,11 @@
+function runningSum(nums: number[]): number[] {
+  const result: number[] = [];
+  let sum: number = 0;
+
+  for (const num of nums) {
+    sum += num;
+    result.push(sum);
+  }
+
+  return result;
+}
